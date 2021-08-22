59f4f4615277d783f37c0e7164663ed0a6dcad05
Andrew Kang <andrewkang.ubc2020@gmail.com>
2021-08-21 19:24:15 -0700

    added file upload and notification support

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, flash, url_for
+from werkzeug.utils import redirect, secure_filename
+from flask_toastr import Toastr
+import time
 
 app = Flask(__name__)
+app.secret_key = 'oAQcsFERTqOq6Iua3hvngkCCq33hgzgRp1nwhBkk9agwiZkNOJ'
+app.config['UPLOAD_FOLDER'] = os.path.abspath("uploads");
+ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
+toastr = Toastr(app)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route('/imageupload', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('file uploaded successfully')
+            return redirect(url_for('home'))
+    flash('Please submit a pdf, png, jpeg, or jpg file.')
+    return redirect(url_for('home'))
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 if __name__ == "__main__":
     app.run(debug=True)
-
