1356b6eaf1703d3732c06fd3b88be96c5335edcf
Andrew Kang <andrewkang.ubc2020@gmail.com>
2021-08-21 21:59:37 -0700

    finished and debugged app.py model.py is an example api class

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import importlib
 from flask import Flask, render_template, request, flash, url_for
 from werkzeug.utils import redirect, secure_filename
 from flask_toastr import Toastr
+from PIL import Image
 
 app = Flask(__name__)
 app.secret_key = 'oAQcsFERTqOq6Iua3hvngkCCq33hgzgRp1nwhBkk9agwiZkNOJ'
 app.config['UPLOAD_FOLDER'] = os.path.abspath("uploads");
-ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 toastr = Toastr(app)
 
 @app.route("/")
@@ -24,11 +25,16 @@ def upload_file():
             file.save(filepath)
             flash('file uploaded successfully')
             if filename.rsplit('.', 1)[1].lower() != 'jpg':
-                importlib.import_module('model')
-                string_output = model.get_translation(filepath)
-                os.remove(filepath)
-                return render_template("index.html", number_guess=string_output)
-    flash('Please submit a pdf, png, jpeg, or jpg file.')
+                my_image = Image.open(filepath)
+                my_image = my_image.convert('RGB')
+                oldfilepath = filepath
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename.rsplit('.', 1)[0] + '.jpg')
+                my_image.save(filepath)
+                os.remove(oldfilepath)
+            model = importlib.import_module('model')
+            string_output = model.get_translation(filepath)
+            return render_template("index.html", number_guess=string_output)
+    flash('Please submit a png, jpeg, or jpg file.')
     return redirect(url_for('home'))
 
 def allowed_file(filename):
