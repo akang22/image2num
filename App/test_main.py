6e9168c774d35e71823c73fe6882abf5c0238ef8
akang22 <68970260+akang22@users.noreply.github.com>
2021-08-22 21:12:18 -0700

    Add allowed_file test

--- App/test_main.py
+++ App/test_main.py
@@ -2,3 +2,6 @@ import main
 
 def test_pipeline
   assert True
+  
+def test_allowed_file(filename):
+  assert allowed_file('asdf.jpg') and allowed_file('asd.f.jpg') and allowed_file('asdf.jpeg') and allowed_file('asdf.png')
