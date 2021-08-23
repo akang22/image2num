2708c90b8fe05e398ee67c299b3e0e07428e7e30
akang22 <68970260+akang22@users.noreply.github.com>
2021-08-22 21:15:53 -0700

    delete test

--- App/test_main.py
+++ App/test_main.py
@@ -1,7 +1,5 @@
 import main
 
-def test_pipeline
+def test_pipeline:
   assert True
-  
-def test_allowed_file(filename):
-  assert allowed_file('asdf.jpg') and allowed_file('asd.f.jpg') and allowed_file('asdf.jpeg') and allowed_file('asdf.png')
+ 
