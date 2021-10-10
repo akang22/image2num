c649bc4a5ee96d5e6bf28b57dc336eb791724fba
akang22 <68970260+akang22@users.noreply.github.com>
2021-10-09 19:51:27 -0700

    Secret key stuff

--- App/main.py
+++ App/main.py
@@ -7,7 +7,7 @@ from PIL import Image
 from nn_utils import transform_image, get_prediction
 
 app = Flask(__name__)
-app.secret_key = 'oAQcsFERTqOq6Iua3hvngkCCq33hgzgRp1nwhBkk9agwiZkNOJ'
+app.secret_key = os.environ['SECRET_KEY'] or "defaultKeyChangeTheEnvironmentVariableIfThisIsProduction"
 app.config['UPLOAD_FOLDER'] = os.path.abspath("uploads");
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 toastr = Toastr(app)
